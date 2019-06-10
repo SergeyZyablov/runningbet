30b74de2c0f7503c95e4b90491302a2c7264073e
sergey <sergey@sergey-ThinkPad-Edge-E531>
2019-06-10 17:55:28 +0300

    Empty project

---
+++ runningbet/src/main/java/ua/runningbet/RunningbetApplication.java
@@ -0,0 +1,13 @@
+package ua.runningbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningbetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningbetApplication.class, args);
+	}
+
+}
